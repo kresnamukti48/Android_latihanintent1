8da46abe524d9fb39da9d944cc553e70a9aa7b0d
madeyahe123 <kresnamukti48@gmail.com>
2020-08-10 21:17:04 +0700

    commit pertama

--- app/src/main/java/com/latihan/intentapp/MainActivity.java
+++ app/src/main/java/com/latihan/intentapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.latihan.intentapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +14,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 		Button btnMoveActivity = findViewById(R.id.btn_move_activity);
 		btnMoveActivity.setOnClickListener(this);
+
 		Button btnMoveWithDataActivity = findViewById(R.id.btn_move_activity_data);
 		btnMoveWithDataActivity.setOnClickListener(this);
+
+		Button btnDialPhone = findViewById(R.id.btn_dial_number);
+		btnDialPhone.setOnClickListener(this);
 
 	}
 
@@ -35,6 +41,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 				startActivity(moveWithDataIntent);
 
 				break;
+
+			case R.id.btn_dial_number:
+				String phoneNumber = "081210841382";
+				Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+				startActivity(dialPhoneIntent);
+
+				break;
+
 		}
 	}
 
